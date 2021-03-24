e74d74ee7b2e3196325dab37747d0b31489167a7
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2021-03-25 01:31:39 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+df -h
+mkdir -p /tmp/rom
+cd /tmp/rom
+repo init -q --no-repo-verify --depth=1 -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 32
+df -h
+du -sh
