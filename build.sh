45d063e6d3f0efbe1f5ba2de510c8683ab742e20
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2021-03-25 01:51:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 df -h
 mkdir -p /tmp/rom
 cd /tmp/rom
-repo init -q --no-repo-verify --depth=1 -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --no-repo-verify --depth=1 -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 32
+ls
+df -h
+sudo apt install neofetch -y
+neofetch
 
