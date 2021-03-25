a8a0c2a5a87bea2277bf0e25a42b7f2cb2145e50
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2021-03-25 08:56:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-df -h
 mkdir -p /tmp/rom
 cd /tmp/rom
-repo init --no-repo-verify --depth=1 -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 32
-ls
-df -h
-sudo apt install neofetch -y
-neofetch
+repo init -u https://github.com/LineageOS/android -b lineage-17.1 -c --depth=1 --groups=default,-device,-pixel,-coral,-marlin,-wahoo,-qcom_sdm845,-darwin -p linux --no-tags  
+repo sync -j16 -c --no-tags --optimized-fetch
+git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_msm8937-common -b 10 --depth=1 vendor/xiaomi/msm8937-common
+git clone https://github.com/mi-msm8937/android_device_xiaomi_msm8937-common -b lineage-17.1 --depth=1 device/xiaomi/msm8937-common
+git clone https://github.com/mi-msm8937/android_device_xiaomi_ulysse -b lineage-17.1 --depth=1 device/xiaomi/ulysse
+git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_ulysse -b lineage-17.1 --depth=1 vendor/xiaomi/ulysse
+git clone https://github.com/mi-msm8937/android_kernel_xiaomi_msm8937-4.9 -b 11 --depth=1 kernel/xiaomi/msm8937-4.9
+git clone https://github.com/mi-msm8937/android_kernel_xiaomi_msm8937-4.9 -b 11 --depth=1 kernel/xiaomi/ulysse-4.9
+brunch ulysse
 
