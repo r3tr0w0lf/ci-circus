8806f2355be226b94aec6923a95fe8607cb7b4e7
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2021-03-25 09:56:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-mkdir -p /tmp/rom
-cd /tmp/rom
-repo init -u https://github.com/LineageOS/android -b lineage-17.1 -c --depth=1 --groups=default,-device,-pixel,-coral,-marlin,-wahoo,-qcom_sdm845,-darwin -p linux --no-tags  
-repo sync -j32 -c --no-tags --optimized-fetch
-git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_msm8937-common -b 10 --depth=1 vendor/xiaomi/msm8937-common
-git clone https://github.com/mi-msm8937/android_device_xiaomi_msm8937-common -b lineage-17.1 --depth=1 device/xiaomi/msm8937-common
-git clone https://github.com/mi-msm8937/android_device_xiaomi_ulysse -b lineage-17.1 --depth=1 device/xiaomi/ulysse
-git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_ulysse -b lineage-17.1 --depth=1 vendor/xiaomi/ulysse
-#git clone https://github.com/mi-msm8937/android_kernel_xiaomi_msm8937-4.9 -b 11 --depth=1 kernel/xiaomi/msm8937-4.9
-git clone https://github.com/mi-msm8937/android_kernel_xiaomi_msm8937-4.9 -b 11 --depth=1 kernel/xiaomi/ulysse-4.9
-source build/envsetup.sh
-breakfast ulysse
-mka bacon -j32 &
+aria2c -x16 https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz
+tar xvf tmate*
+./tmate*/tmate -F > info
 for (( ; ; ))
 do
-   du -sh
+   cat info
    sleep 10m
 done
 
