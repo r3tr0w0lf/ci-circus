d3af61170b5b40c22e0890955d05f2c8a466c4de
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2021-03-25 09:00:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir -p /tmp/rom
 cd /tmp/rom
 repo init -u https://github.com/LineageOS/android -b lineage-17.1 -c --depth=1 --groups=default,-device,-pixel,-coral,-marlin,-wahoo,-qcom_sdm845,-darwin -p linux --no-tags  
-repo sync -j16 -c --no-tags --optimized-fetch
+repo sync -j32 -c --no-tags --optimized-fetch
 git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_msm8937-common -b 10 --depth=1 vendor/xiaomi/msm8937-common
 git clone https://github.com/mi-msm8937/android_device_xiaomi_msm8937-common -b lineage-17.1 --depth=1 device/xiaomi/msm8937-common
 git clone https://github.com/mi-msm8937/android_device_xiaomi_ulysse -b lineage-17.1 --depth=1 device/xiaomi/ulysse
