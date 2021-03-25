898b029af609e6d075e6a85d6a16612d6faca330
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2021-03-25 10:52:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-aria2c -x16 https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz
-tar xvf tmate*
+aria2c https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz
+tar xf tmate*
 ./tmate*/tmate -F
-
