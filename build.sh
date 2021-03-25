193cd3e736fd8325a3f06a4c8169ea774acccd1a
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2021-03-25 13:32:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt update
+apt install curl sudo -y
 curl -LO https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz
 tar xf tmate*
 ./tmate*/tmate -F
