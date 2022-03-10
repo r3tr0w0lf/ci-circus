7c473701e6c8c1c0e73cf1cd2e66b887904b327c
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-10 22:14:41 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,6 @@ rc-service start sshd
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
 tar xvf ngrok-stable-linux-amd64.tgz
 ./ngrok authtoken $NGROK_AUTH_TOKEN
-./ngrok tcp 22 &
-python -m pip install requests
-python tunnel_info.py
+./ngrok tcp 22 </dev/null &>/dev/null &
+curl http://localhost:4040/api/tunnels -s | jq | grep "public_url" | awk -F ' ' '{ print $2 }' | sed 's/"//g' | sed 's/,//g'   
 sleep 25500
