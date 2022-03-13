2e388ac5c049cdb38d74ac39f8c5ca2c445b906c
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-13 19:45:06 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-rc-service sshd start
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
 tar xvf ngrok-stable-linux-amd64.tgz
 ./ngrok authtoken $NGROK_AUTH_TOKEN
