5cc49a91209b5f0b0bc28d68a74ba167c04f27a7
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-13 21:00:33 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,7 +15,7 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz --quiet
 tar xf ngrok-stable-linux-amd64.tgz 
 ./ngrok authtoken $NGROK_AUTH_TOKEN &>/dev/null
 ./ngrok tcp 22 </dev/null &>/dev/null &
-emerge app-misc/jq &>/dev/null
+emerge app-misc/jq --quiet
 echo -e "\n\n"
 while true
 do
