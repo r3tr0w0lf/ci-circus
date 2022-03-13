9ebdb28639584392c930a5a6f761bf9f6f90a79e
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-13 20:13:59 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 
 rc-service sshd start 
 sh -c 'echo root:R3tr0w0lfavi=1229 | chpasswd'
-sed 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g;s/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+ls -lah /root/
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g;s/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz --quiet
 tar xf ngrok-stable-linux-amd64.tgz 
 ./ngrok authtoken $NGROK_AUTH_TOKEN
