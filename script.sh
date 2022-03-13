f031c9fb3c92f223bde9c498da3f844c2e26b10b
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-13 20:45:44 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # SSH SETUP
-ssh-keygen -A
+ssh-keygen -A &>/dev/null
 /usr/sbin/sshd -o PidFile=/run/sshd.pid -f /etc/ssh/sshd_config
 mkdir /root/.ssh -p
 chmod 700 /root/.ssh
@@ -13,7 +13,7 @@ sh -c 'echo root:R3tr0w0lfavi=1229 | chpasswd' &>/dev/null
 # NGROK SETUP
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz --quiet
 tar xf ngrok-stable-linux-amd64.tgz 
-./ngrok authtoken $NGROK_AUTH_TOKEN
+./ngrok authtoken $NGROK_AUTH_TOKEN &>/dev/null
 ./ngrok tcp 22 </dev/null &>/dev/null &
 sleep 3
 curl -s http://127.0.0.1:4040/api/tunnels 
