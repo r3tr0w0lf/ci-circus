ef2e0fbae5db4dee607a27c4c5ca82de475f7e5c
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-13 20:31:34 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sshd
+ps aux | grep -i sshd
 sh -c 'echo root:R3tr0w0lfavi=1229 | chpasswd'
 mkdir /root/.ssh -p
 chmod 700 /root/.ssh
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/HD4ykICHel8LtrbicOw7raeaczAvbLoztNUyiKsWF u0_a113@localhost" >> /root/.ssh/authorized_keys
-#sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g;s/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g;s/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz --quiet
 tar xf ngrok-stable-linux-amd64.tgz 
 ./ngrok authtoken $NGROK_AUTH_TOKEN
