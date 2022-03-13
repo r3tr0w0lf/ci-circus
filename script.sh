9402340c11f6d09bd67fa81be6cdb12f1fc1f226
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-13 20:43:39 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
+# SSH SETUP
 ssh-keygen -A
 /usr/sbin/sshd -o PidFile=/run/sshd.pid -f /etc/ssh/sshd_config
-ps aux | grep -i sshd
-sh -c 'echo root:R3tr0w0lfavi=1229 | chpasswd'
 mkdir /root/.ssh -p
 chmod 700 /root/.ssh
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/HD4ykICHel8LtrbicOw7raeaczAvbLoztNUyiKsWF u0_a113@localhost" >> /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/*
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g;s/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sh -c 'echo root:R3tr0w0lfavi=1229 | chpasswd' &>/dev/null
+
+# NGROK SETUP
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz --quiet
 tar xf ngrok-stable-linux-amd64.tgz 
 ./ngrok authtoken $NGROK_AUTH_TOKEN
 ./ngrok tcp 22 </dev/null &>/dev/null &
 sleep 3
-curl http://127.0.0.1:4040/api/tunnels 
+curl -s http://127.0.0.1:4040/api/tunnels 
 sleep 25500
