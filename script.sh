75a605c3560f73c1e0058ef07e72eb3b157b99bf
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-13 19:55:02 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,5 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz --quiet
 tar xf ngrok-stable-linux-amd64.tgz 
 ./ngrok authtoken $NGROK_AUTH_TOKEN
 ./ngrok tcp 22 </dev/null &>/dev/null &
-ls
-curl http://localhost:4040/api/tunnels 
+curl http://127.0.0.1:4040/api/tunnels 
 sleep 25500
