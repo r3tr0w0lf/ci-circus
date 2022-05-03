7412e21326a9b369f283b7ac9988748c81d06991
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-05-03 18:38:14 +0530

    [no ci]

--- script.sh
+++ script.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # SSH SETUP
-sudo mkdir /root/.ssh -p
-sudo chmod 700 /root/.ssh
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzbb76/TEhlX8iPth0+ZppUmnXlKuY64iNqInPntJix x0rzavi@gmail.com" | sudo tee /root/.ssh/authorized_keys
-sudo chmod 600 /root/.ssh/*
-sudo sh -c 'echo root:R3tr0w0lfavi=1229 | chpasswd' 
+mkdir ~/.ssh -p
+chmod 700 ~/.ssh
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzbb76/TEhlX8iPth0+ZppUmnXlKuY64iNqInPntJix x0rzavi@gmail.com" > ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/*
+echo "runner:1229" | sudo chpasswd 
 
 # NGROK SETUP
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz --quiet
-tar xf ngrok-stable-linux-amd64.tgz 
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz --quiet | tar xf
 ./ngrok authtoken $NGROK_AUTH_TOKEN
 ./ngrok tcp 22 </dev/null &
 
+# SHOW URL
 while true
 do
   curl http://localhost:4040/api/tunnels -s | jq | grep "public_url" | awk -F ' ' '{ print $2 }' | sed 's/"//g;s/,//g;s/tcp:\/\///g;s/:/ -p/g'
