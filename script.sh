b9c481de7132b030434a2f33ee49663ba51cefae
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-05-03 18:33:23 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 # SSH SETUP
 sudo mkdir /root/.ssh -p
 sudo chmod 700 /root/.ssh
-sudo echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzbb76/TEhlX8iPth0+ZppUmnXlKuY64iNqInPntJix x0rzavi@gmail.com" >> /root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzbb76/TEhlX8iPth0+ZppUmnXlKuY64iNqInPntJix x0rzavi@gmail.com" | sudo tee /root/.ssh/authorized_keys
 sudo chmod 600 /root/.ssh/*
 sudo sh -c 'echo root:R3tr0w0lfavi=1229 | chpasswd' 
 
